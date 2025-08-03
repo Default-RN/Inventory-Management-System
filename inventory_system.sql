6d7373ca1ee2794ac0893e03700d4241e6b68eb1
Run0103Codes <101922955+Default-RN@users.noreply.github.com>
2025-08-03 14:55:20 +0530

    Update inventory_system.sql

--- inventory_system.sql
+++ inventory_system.sql
@@ -1,29 +1,9 @@
--- phpMyAdmin SQL Dump
--- version 3.3.9
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Aug 03, 2025 at 09:19 AM
--- Server version: 5.5.8
--- PHP Version: 5.3.5
+
+-- This is optional if someone is trying to use sql using wamp server 
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
 -- Database: `inventory_system`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `category_data`
---
 
 CREATE TABLE IF NOT EXISTS `category_data` (
   `id` int(11) NOT NULL,
